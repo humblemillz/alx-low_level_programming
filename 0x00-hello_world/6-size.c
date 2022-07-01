f457273c1580f899cd6daab8792674ec60ecc004
humblemillz <humblemillions@gmail.com>
2022-06-30 17:29:29 -0700

    6-size.c push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *
- * main - Entry point
- *
- * Return: always 0
- *
- */
+*
+* main - Entry point
+*
+* Return: always 0
+*
+*/
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
