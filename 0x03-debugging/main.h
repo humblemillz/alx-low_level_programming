cfc7a8b264bdbebb64644d6ce9e47e0c7c4d6654
humblemillz <humblemillions@gmail.com>
2022-07-08 07:10:10 -0700

    2-largest_number.c1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,8 @@
 #define MAIN_H
 
 #include <studio.h>
-
 void positive_or_negative(int i);
 void print_remaining_days(int, int, int);
 int convert_day(int month, int day);
 nt largest_number(int a, int b, int c);
-
 #endif
