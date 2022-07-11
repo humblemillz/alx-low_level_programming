2bb4956a8cde8861c0c1acb74464c51885e9384d
humblemillz <humblemillions@gmail.com>
2022-07-11 01:42:19 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
- * main - function to print
- *
+ * main - prints a string of characters
+ * Description: prints _putchar, followed by a new line
  * Return: 0
  */
 int main(void)
 {
-
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+char a[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+return (0);
 }
