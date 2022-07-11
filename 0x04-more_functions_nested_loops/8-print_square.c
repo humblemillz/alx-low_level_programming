803c9e1af7a91638d9c96da89e6c74f2cafd374b
humblemillz <humblemillions@gmail.com>
2022-07-11 08:26:12 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,19 @@
 
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	if (size == 0 || size < 0)
-		_putchar('\n');
-	else
+	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
-			_putchar('#');
+		for (i = 0; i <	size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
