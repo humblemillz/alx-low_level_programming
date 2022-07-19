48b4341df25bc5a800280fbd02489fad593baf04
humblemillz <humblemillions@gmail.com>
2022-07-19 10:06:02 -0700

    8-print_diagsums.c corrected

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,16 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - prints the chessboard.
- * @a: the board
- */
+* print_chessboard - prints the chessboard
+* @a: the board to print
+*/
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-for (i = 0; i < 8; i++)
+int c, d;
+c = d = 0;
+while (c < 8)
 {
-for (j = 0; j < 8; j++)
-_putchar(a[i][j]);
+d = 0;
+while (d < 8)
+{
+_putchar(a[c][d]);
+d++;
+}
 _putchar('\n');
+c++;
 }
 }
