bcca3a9e91f84722c7d0fbf4ab1ee6c8f6db1f73
humblemillz <humblemillions@gmail.com>
2022-07-20 14:45:04 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,12 +13,12 @@ char *wildcard(char *, char *);
 int wildcmp(char *s1, char *s2)
 {
 	char *p;
-	if (*s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	else if (*s2 == '*')
-	{
+if (*s2 == '*' && *(s2 + 1) == '\0')
+return (1);
+else if (*s2 == '*' && *(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+else if (*s2 == '*')
+{
 		p = wildcard(s1, s2 + 1);
 		if (p == 0)
 			return (0);
